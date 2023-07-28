6ffe75f82859f1ea1d55217a1c600b66830b5782
Fernando Granados Juárez <fernandograjua@gmail.com>
2023-07-28 20:26:53 +0200

    ADD - Added Components & interface of Note ADD - Modules MOD - Change package.json "start"

--- SpeedyNotesWebUI/src/app/app.component.ts
+++ SpeedyNotesWebUI/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Note} from "./model/note";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SpeedyNotesWebUI';
+
+  notes:Note[]  = [
+    {title:"hoal",subTitle:"",content:""},
+    {title:"hoal",subTitle:"",content:""}
+  ]
+
 }
